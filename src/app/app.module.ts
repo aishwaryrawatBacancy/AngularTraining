6cf39574397b974abfe3582ce9417f6c51545220
Aishwary Rawat <aishwary.rawat@moldev.com>
2022-11-04 18:20:52 +0530

    Directives deep dive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { HighlightCardDirective } from './shared/directives/highlight-card.directive';
+import { UnlessDirective } from './shared/directives/structural-directive/unless.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    HighlightCardDirective,
+    UnlessDirective
   ],
   imports: [
     BrowserModule
