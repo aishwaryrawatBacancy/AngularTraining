a2292f4faedb182a16a2ca11415ceb711ca88c0c
harharmahadev <hardikshyara37@gmail.com>
2022-11-07 14:41:02 +0530

    Custome directive dropdown

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private elem: ElementRef
+  ) { }
 
   ngOnInit(): void {
   }
+  title = 'Dropdown';
+  public dropdownOpen: boolean = false;
+  public get dropdownElement(): Element {return this.elem.nativeElement.querySelector('.dropdown-list')}
+
+  public currentValue:any;
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+    this.dropdownElement.setAttribute('aria-expanded', this.dropdownOpen ? "true" : "false");
+}
 
 }
